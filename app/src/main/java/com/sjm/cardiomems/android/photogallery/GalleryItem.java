c19200e26f3d36451f1bbb4bc50dfc93967050ae
Shawn Veader <shawn@veader.org>
2015-08-27 11:29:52 -0700

    Chapter 23: Finished

---
+++ app/src/main/java/com/sjm/cardiomems/android/photogallery/GalleryItem.java
@@ -0,0 +1,38 @@
+package com.sjm.cardiomems.android.photogallery;
+
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+
+}
---
+++ app/src/main/java/com/sjm/cardiomems/android/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,10 @@
+package com.sjm.cardiomems.android.photogallery;
+
+import android.support.v4.app.Fragment;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+}
